00e8427515662804c73c6414fff4fc11ce05426c
Nolan Kovacik <noltron000@gmail.com>
2018-10-29 12:50:18 -0700

    fixing dictionary definition

--- mini/wordify.py
+++ mini/wordify.py
@@ -1,9 +1,11 @@
 import rearrange
 
-def wordify():
-	file = open("/usr/share/dict/words","r")
+def dictionary_main():
+	file = open("/usr/share/dict/words", "r")
 	text = file.read()
 	file.close()
+	return text
 
 if __name__ == '__main__':
+	text = dictionary_main()
 	print(rearrange.rearrange(text, 20))
