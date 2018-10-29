29f00df2ecfe9189c826745ca7886833ee24cb46
Nolan Kovacik <noltron000@gmail.com>
2018-10-29 16:43:04 -0700

    refactoring code

---
+++ mini/counter.py
@@ -0,0 +1,11 @@
+'''
+	Read dictionary:
+	strip strings of everything but alpha characters.
+	For each item:
+		check if name-counter exists.
+			If it doesnt, make it exist.
+		add one to the name-counter.
+	return name-counter object
+'''
+
+def histogram(text):
---
+++ mini/dictionary.py
@@ -0,0 +1,23 @@
+import rearrange
+import anagrams
+import palindromes
+
+def dictionary_main():
+	file = open("/usr/share/dict/words", "r")
+	text = file.read()
+	file.close()
+	return text
+
+if __name__ == '__main__':
+	text = dictionary_main()
+	print("\nhere's rearranged text")
+	print("======================")
+	print(rearrange.rearrange(text, 20))
+
+	print("\nhere's palindrome text")
+	print("======================")
+	print(palindromes.palindromify_data(text))
+
+	print("\nhere's anagramify text")
+	print("======================")
+	print(anagrams.anagramify_data(text,input("enter word: >> ")))
