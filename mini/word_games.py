e6299561e36a36a703552a352ccb9cdae5b6ef4c
Nolan Kovacik <noltron000@gmail.com>
2018-10-30 11:01:12 -0700

    histogram works

--- mini/word_games.py
+++ mini/word_games.py
@@ -31,7 +31,7 @@ def user_string(feed):
 	while True:
 		try:
 			string = str(input("enter string:\n>> "))
-			assert isinstance(integer, str) # throws an error if its not an string
+			assert isinstance(string, str) # throws an error if its not an string
 			return string
 		except:
 			print("INVALID INPUT.\nplease try again.\n")
@@ -48,14 +48,15 @@ def listify_data(input_data):
 	assert type(input_data) is list
 	return input_data
 
-def randify_list(input_list, max_length=user_number("\nI generate several words.\nHow many should I create?")):
+def randify_list(input_list, iterations):
 	# randomly shuffles a list.
 	'''
 		picks a random digit, min 0 max current length of input list
 		pops current digit into output list
+		the `iterations` variable determines the number of words to output
 	'''
 	output_list = []
-	while len(input_list) > 0 and (len(output_list) < max_length or max_length == 0):
+	while len(input_list) > 0 and (len(output_list) < iterations or iterations == 0):
 		digit_rand = random.randint(0, len(input_list) - 1)
 		input_rand = input_list.pop(digit_rand)
 		output_list += [input_rand]
@@ -76,6 +77,17 @@ def textify_list(input_list):
 	input_list = output_text
 	return input_list
 
+def lowerfy_list(input_list):
+	# takes a list of strings and runs lowercase on each item.
+	'''
+		output_text is the final return product.
+		each item is added with a space to output_text.
+		the final output_text string is returned
+	'''
+	for string in input_list:
+		string = string.lower()
+	return input_list
+
 def reverse_text(old_word):
 	# simply reverses a text.
 	new_word = ""
@@ -92,16 +104,41 @@ def reverse_text(old_word):
 '''
 ###
 
-def rearrange():
-	data = dictionary_main()
+def rearrange(iterations, input_data = dictionary_main()):
+	data = input_data
 	data = listify_data(data)
-	data = randify_list(data)
+	data = lowerfy_list(data)
+	data = randify_list(data, iterations)
 	data = textify_list(data)
-	print("\n" + data)
+	return(data)
+
+### ANAGRAMS
+
+def anagrams (input_word, input_data = dictionary_main()): # anagrange = anagram arrange
+	data = input_data
+	data = listify_data(data)
+	data = lowerfy_list(data)
+	new_data = []
+	i_word = input_word
+	for v_word in data:
+		if verify_anagram(i_word,v_word):
+			new_data += [v_word]
+	new_data = textify_list(new_data)
+	return new_data
+
+def verify_anagram(input_word, verify_word):
+	input_list = list(input_word)
+	verify_list = list(verify_word)
+	input_list.sort()
+	verify_list.sort()
+	if input_list == verify_list:
+		return True
+	else:
+		return False
 
 ### PALINDROMES
 
-def palindromify_data(input_data):
+def palindromes(input_data = dictionary_main()):
 	data = input_data
 	data = listify_data(data)
 	new_data = []
@@ -119,47 +156,45 @@ def verify_palindrome(input_word):
 		i += 1
 	return True
 
-### ANAGRAMS
-
-def anagrams(input_data, input_word): # anagrange = anagram arrange
-	data = input_data
-	data = rearrange.listify_data(data)
-	i_word = input_word
-	new_data = []
-	for v_word in data:
-		# print(f"\nanagram: {i_word}\nversus: {v_word}")
-		if verify_anagram(i_word,v_word):
-			new_data += [v_word]
-	new_data = rearrange.textify_list(new_data)
-	return new_data
-
-def verify_anagram(input_word, verify_word):
-	input_list = list(input_word)
-	verify_list = list(verify_word)
-	input_list.sort()
-	verify_list.sort()
-	if input_list == verify_list:
-		return True
-	else:
-		return False
+# HISTOGRAM
 
 '''
 	Read dictionary:
 	strip strings of everything but alpha characters.
-	For each item:
-		check if name-counter exists.
+	For each word in data:
+		check if name-counter exists in hist.
 			If it doesnt, make it exist.
 		add one to the name-counter.
 	return name-counter object
 '''
 
+def histogram(input_data = dictionary_main()):
+	data = listify_data(input_data)
+	data = lowerfy_list(data)
+	hist = {}
+	for word in data:
+		found = False
+		if word in hist:
+			found = True
+		if found:
+			hist[word] += 1
+		else:
+			hist[word] = 1
+	output = ""
+	for w in sorted(hist, key=hist.get, reverse=False):
+		if hist[w] > 1:
+			output += str(hist[w]) + ' ' + str(w) + "\n"
+	return output
+
 ### STARTER KIT
 
 if __name__ == '__main__':
-	print("\n================")
-	print("Generating Words")
-	rearrange()
+	# entry = user_number("\nI generate several words.\nHow many should I create?")
+	print("\nRandom Words:\n" + rearrange(4))
 
-	print("\n===================")
-	print("Generating Anagrams")
-	anagrams()
+	# entry = user_string("\nI make anagrams, so I need a word.\nWhat base word should I use today?")
+	print("\nAnagrams:\n" + anagrams("super"))
+
+	print("\nPalindromes:\n" + palindromes())
+
+	print("\nHistogram:\n" + histogram())
